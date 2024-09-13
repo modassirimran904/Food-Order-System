e4fff9e12cd234c53f7f589710a14a44a2678249
Modassir Imran <96190246+modassirimran904@users.noreply.github.com>
2024-09-13 22:04:22 +0530

    Update Home

--- forntend/src/screen/Home.jsx
+++ forntend/src/screen/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   const [foodItems, setFoodItems] = useState([])
 
   const loadData = async () => {
-    let response = await fetch('http://localhost:5000/api/foodData', {
+    let response = await fetch('https://food-order-system-six.vercel.app/api/foodData', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
