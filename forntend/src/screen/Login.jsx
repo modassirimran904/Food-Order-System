0d45b01c65a73f5a9511cb4487a4d20edab96142
Modassir Imran <96190246+modassirimran904@users.noreply.github.com>
2024-09-13 22:11:49 +0530

    Update Login.jsx

--- forntend/src/screen/Login.jsx
+++ forntend/src/screen/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
 
   const handleSubmit = async e => {
     e.preventDefault()
-    const response = await fetch('http://localhost:5000/api/loginuser', {
+    const response = await fetch('https://food-order-system-six.vercel.app/api/loginuser', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
