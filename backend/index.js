f80a15bc3ccf89cd7d6b778648c71f60a755beee
Modassir Imran <modassirimran904@gmail.com>
2024-09-13 21:55:51 +0530

    vert set

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,45 @@
-const express = require('express')
+  const express = require('express')
 
-const cors = require('cors')
-const app = express()
-const dotenv = require('dotenv');
-dotenv.config();
+  const cors = require('cors')
+  const app = express()
+  const dotenv = require('dotenv');
+  dotenv.config();
 
-app.use(cors(
-  {
-    origin:[],
-    method:['POST','GET'],
-    credential:true
-  }
-))
+  app.use(cors(
+    {
+      origin:[],
+      method:['POST','GET'],
+      credential:true
+    }
+  ))
 
-const mongoDB = require('./database')
-mongoDB()
+  const mongoDB = require('./database')
+  mongoDB()
 
-const port = process.env.PORT || 6000
+  const port = process.env.PORT || 6000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
-// Corrected Middleware for CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
+  // Corrected Middleware for CORS
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
 
-app.use(express.json())
-app.use('/api', require('./routes/CreateUser'))
-app.use('/api', require('./routes/DisplayData'))
-app.use('/api', require('./routes/OrdersData'))
-app.use('/api', require('./routes/MyOrderData'))
+  app.use(express.json())
+  app.use('/api', require('./routes/CreateUser'))
+  app.use('/api', require('./routes/DisplayData'))
+  app.use('/api', require('./routes/OrdersData'))
+  app.use('/api', require('./routes/MyOrderData'))
 
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
